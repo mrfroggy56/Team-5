affd90d3b229045dea4aef1a6bf6325903cd13c8
ThatDevBoi <davebrooks199909@gmail.com>
2019-03-15 11:31:52 +0000

    Putting things together
    This is just editing the playable scenes and connecting a Main Menu to the other scenes

---
+++ Rhythm Buster/Assets/DB_Change_Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DB_Change_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeScene(string _SceneName)
+    {
+        SceneManager.LoadScene(_SceneName);
+    }
+}
---
+++ Rhythm Buster/Assets/DB_End_Application.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DB_End_Application : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Ended");
+    }
+}
